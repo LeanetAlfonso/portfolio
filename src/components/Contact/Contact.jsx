72bbe29404093477945fe192542c87c2aa2440d6
LeanetAlfonso <leanet.alfonso1@gmail.com>
2022-10-01 14:29:26 -0400

    Update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Contact.css';
 import Caption from '../Caption/Caption';
 import {useNavigate} from 'react-router-dom';
@@ -9,18 +9,21 @@ const encode = (data) => {
 		.join('&');
 };
 
-class Contact extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {name: '', email: '', message: ''};
-	}
+const Contact = () => {
+	const [formData, setFormData] = useState({name: '', email: '', message: ''});
 
-	handleSubmit = (e) => {
-		let navigate = useNavigate();
+	// const [name, setName] = useState('');
+	// const [email, setEmail] = useState('');
+	// const [message, setMessage] = useState('');
+
+	let navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		console.log(...formData);
 		fetch('/', {
 			method: 'POST',
 			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-			body: encode({'form-name': 'contact', ...this.state}),
+			body: encode({'form-name': 'contact', ...formData}),
 		})
 			.then(() => navigate('/thank-you/'))
 			.catch((error) => alert(error));
@@ -28,73 +31,80 @@ class Contact extends React.Component {
 		e.preventDefault();
 	};
 
-	handleChange = (e) => this.setState({[e.target.name]: e.target.value});
+	const handleChange = (e) => {
+		setFormData({...formData, [e.target.name]: e.target.value});
+	};
 
-	render() {
-		const {name, email, message} = this.state;
+	return (
+		<section id='contact'>
+			<div className='container content__container about__container contact__container'>
+				<Caption caption='Get In Touch' icon='contact' />
+				<h3 className='text-light'>Let's work together!</h3>
+				<form
+					id='contact-form'
+					onSubmit={handleSubmit}
+					data-netlify='true'
+					name='contactv1'
+					method='POST'
+					action='/thank-you/'
+					netlify-honeypot='bot-field'
+				>
+					<input type='hidden' name='form-name' value='contactv1' />
+					<div hidden>
+						<input name='bot-field' value='' readOnly />
+					</div>
 
-		return (
-			<section id='contact'>
-				<div className='container content__container about__container contact__container'>
-					<Caption caption='Get In Touch' icon='contact' />
-					<h3 className='text-light'>Let's work together!</h3>
-					<form
-						onSubmit={this.handleSubmit}
-						data-netlify='true'
-						name='contactv1'
-						method='POST'
-						netlify-honeypot='bot-field'
-					>
-						<input type='hidden' name='form-name' value='contactv1' />
-						<div hidden>
-							<input name='bot-field' value='' readOnly />
+					<div className='fields'>
+						<div className='field'>
+							<input
+								title='your name'
+								type='text'
+								name='name'
+								id='name'
+								placeholder='Name *'
+								required
+								// value={name}
+								// onChange={(e) => setName(e.target.value)}
+								value={formData.name}
+								onChange={handleChange}
+							/>
 						</div>
-
-						<div className='fields'>
-							<div className='field'>
-								<input
-									title='your name'
-									type='text'
-									name='name'
-									id='name'
-									placeholder='Name *'
-									required
-									value={name}
-									onChange={this.handleChange}
-								/>
-							</div>
-							<div className='field'>
-								<input
-									title='your email'
-									type='email'
-									name='email'
-									id='email'
-									placeholder='Email *'
-									required
-									value={email}
-									onChange={this.handleChange}
-								/>
-							</div>
-							<div className='field'>
-								<textarea
-									title='your message'
-									name='message'
-									id='message'
-									placeholder='Message *'
-									rows='6'
-									required
-									value={message}
-									onChange={this.handleChange}
-								/>
-							</div>
+						<div className='field'>
+							<input
+								title='your email'
+								type='email'
+								name='email'
+								id='email'
+								placeholder='Email *'
+								required
+								// value={email}
+								// onChange={(e) => setEmail(e.target.value)}
+								value={formData.email}
+								onChange={handleChange}
+							/>
 						</div>
-						<button className='btn btn-primary btn-submit' type='submit'>
-							Send Message
-						</button>
-					</form>
-				</div>
-			</section>
-		);
-	}
-}
+						<div className='field'>
+							<textarea
+								title='your message'
+								name='message'
+								id='message'
+								placeholder='Message *'
+								rows='6'
+								required
+								// value={message}
+								// onChange={(e) => setMessage(e.target.value)}
+								value={formData.message}
+								onChange={handleChange}
+							/>
+						</div>
+					</div>
+					<button className='btn btn-primary btn-submit' type='submit'>
+						Send Message
+					</button>
+				</form>
+			</div>
+		</section>
+	);
+};
+
 export default Contact;
